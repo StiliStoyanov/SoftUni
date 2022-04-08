28a2a4b252aeffed3f1742c016f4902ac6742720
Stiliyan Stoyanov <stilistoyanov1@gmail.com>
2022-04-08 08:04:43 +0300

    Create 05. Math Operations.js

---
+++ JS advanced/Syntax, Functions and Statements - Lab/05. Math Operations.js
@@ -0,0 +1,19 @@
+function solve(num1, num2, string) {
+  let result;
+  switch (string) {
+    case "+":
+      result = num1 + num2; break;
+    case "-":
+      result = num1 - num2; break;
+    case "*":
+      result = num1 * num2; break;
+    case "/":
+      result = num1 / num2; break;
+    case "%":
+      result = num1 % num2; break;
+    case "**":
+      result = num1 ** num2; break;
+  }
+  console.log(result);
+}
+solve(3, 5.5, "*");
