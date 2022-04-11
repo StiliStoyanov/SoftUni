fa9452a30eab28ee05b361f99764f7a4dabb85a4
Stiliyan Stoyanov <stilistoyanov1@gmail.com>
2022-04-11 16:02:19 +0300

    Create 09. Square of Stars.js

---
+++ JS advanced/Syntax, Functions and Statements - Lab/09. Square of Stars.js
@@ -0,0 +1,11 @@
+function solve(input) {
+    let result = ''
+    for (let i = 0; i < input; i++) {
+        result += '* '
+    }
+    for (let j = 0; j < input; j++) {
+       console.log(result);
+    }
+
+}
+solve(5)
