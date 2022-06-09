75a59f74f44bd322b0d13ecd5c7b665201753e08
Stiliyan Stoyanov <stilistoyanov1@gmail.com>
2022-06-09 22:37:19 +0300

    DOM lab first task

---
+++ JS advanced/DOM Introduction - Lab/01.Edit Element/EditElement.js
@@ -0,0 +1,8 @@
+function editElement(element, match, replacer) {
+    
+    const text = element.textContent
+    const result = text.split(match).join(replacer)
+    element.textContent = result
+
+    // TODO
+}
