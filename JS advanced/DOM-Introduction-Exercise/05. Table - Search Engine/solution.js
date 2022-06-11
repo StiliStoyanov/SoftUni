f9287cd6606be800bde6581a5bc7b90169259dcc
Stiliyan Stoyanov <stilistoyanov1@gmail.com>
2022-06-11 17:23:54 +0300

    Update solution.js

--- JS advanced/DOM-Introduction-Exercise/05. Table - Search Engine/solution.js
+++ JS advanced/DOM-Introduction-Exercise/05. Table - Search Engine/solution.js
@@ -1,8 +1,15 @@
 function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
+  document.querySelector("#searchBtn").addEventListener("click", onClick);
+  let input = document.getElementById("searchField");
+  let rows = document.querySelectorAll("tbody tr");
 
-   function onClick() {
-      //   TODO:
-
-   }
+  function onClick() {
+    for (let row of rows) {
+      row.classList.remove("select");
+      if (input.value !== "" && row.textContent.includes(input.value)) {
+        row.className = "select";
+      }
+    }
+    input.value = "";
+  }
 }
