69b101652800c91fefd1e8899ea56afb6025c433
Stiliyan Stoyanov <stilistoyanov1@gmail.com>
2022-06-11 13:57:28 +0300

    Update accordion.js

--- JS advanced/DOM-Introduction-Exercise/03. Accordion/accordion.js
+++ JS advanced/DOM-Introduction-Exercise/03. Accordion/accordion.js
@@ -1,3 +1,11 @@
 function toggle() {
-    console.log('TODO:...');
+  let buttonText = document.querySelector(".button").textContent;
+
+  if (buttonText == "More") {
+    document.querySelector(".button").textContent = "Less";
+    document.querySelector("#extra").style.display = "block";
+  } else {
+    document.querySelector("#extra").style.display = "none";
+    document.querySelector(".button").textContent = "More";
+  }
 }
