f70a1a8327e2c2394e56c45826266a1afb1bec63
Stiliyan Stoyanov <stilistoyanov1@gmail.com>
2022-07-08 17:28:02 +0300

    Update app.js

--- JS advanced/Asynchronous Programming - Exercise/01.Bus-Stop/app.js
+++ JS advanced/Asynchronous Programming - Exercise/01.Bus-Stop/app.js
@@ -1,3 +1,26 @@
-function getInfo() {
-    console.log("TODO...");
+async function getInfo() {
+    const stopNameElement  = document.getElementById('stopName')
+    const timetableElements  = document.getElementById('buses')
+
+    const inputField = document.getElementById('stopId').value
+    const url = `http://localhost:3030/jsonstore/bus/businfo/${inputField}` 
+
+
+  
+
+  try {
+    timetableElements.innerHTML = ''
+    const res = await fetch(url)
+    const data = await res.json()
+
+    stopNameElement.textContent = data.name 
+
+    Object.entries(data.buses).forEach(b=>{
+        const listElement = document.createElement('li');
+        listElement.textContent = `Bus ${b[0]} arrives in ${b[1]} minutes`
+        timetableElements.appendChild(listElement)
+    })
+  } catch (error) {
+    stopNameElement.textContent = 'Erorr' 
+  }
 }
