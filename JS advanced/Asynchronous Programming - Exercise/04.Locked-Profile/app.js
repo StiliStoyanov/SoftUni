9d8bb8682e65bb1fb478ec2ac46260f42dd4e1a5
Stiliyan Stoyanov <stilistoyanov1@gmail.com>
2022-07-10 20:10:17 +0300

    Update app.js

--- JS advanced/Asynchronous Programming - Exercise/04.Locked-Profile/app.js
+++ JS advanced/Asynchronous Programming - Exercise/04.Locked-Profile/app.js
@@ -1,3 +1,60 @@
-function lockedProfile() {
-    console.log('TODO...')
+async function lockedProfile() {
+    const url = `http://localhost:3030/jsonstore/advanced/profiles`;
+    const main = document.getElementById('main')
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    main.innerHTML = ''
+    
+    Object.entries(data).forEach(person => {
+        console.log(person);
+        const div = document.createElement('div')
+        div.className = 'profile'
+        div.innerHTML= `<img src="./iconProfile2.png" class="userIcon" />
+        <label>Lock</label>
+        <input type="radio" name="user1Locked" value="lock" checked>
+        <label>Unlock</label>
+        <input type="radio" name="user1Locked" value="unlock"><br>
+        <hr>
+        <label>Username</label>
+        <input type="text" name="user1Username" value="${person[1].username}" disabled readonly />
+        <div class="user1Username" style="display:none;">
+            <hr>
+            <label>Email:</label>
+            <input type="email" name="user1Email" value="${person[1].email}" disabled readonly />
+            <label>Age:</label>
+            <input type="text" name="user1Age" value="${person[1].age}" disabled readonly />
+        </div>
+        
+        <button>Show more</button>`
+        main.appendChild(div)
+        
+
+    })
+     let btns = Array.from(document.querySelectorAll('button'))
+    for (const btn of btns) {
+        
+        btn.addEventListener('click', (e)=> {
+            const divToHide = e.target.previousElementSibling
+            const lockedButton = e.target.parentElement.children[2]
+            if (lockedButton.checked) {
+                
+            }
+            else{
+                if (e.target.textContent == 'Show more') {
+                    divToHide.style.display = 'block'
+                    e.target.textContent = 'Hide it'
+                }
+                else if(e.target.textContent == 'Hide it'){
+                    divToHide.style.display = 'none'
+                    e.target.textContent = 'Show more'
+                }
+            }
+
+          
+        })
+    }
+    
+       
 }
