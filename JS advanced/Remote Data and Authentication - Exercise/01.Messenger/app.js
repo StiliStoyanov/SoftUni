ddbedbc0be3cb3c98f8804180e7550bddf3f661e
Stiliyan Stoyanov <stilistoyanov1@gmail.com>
2022-07-12 16:09:53 +0300

    Added the first task

--- JS advanced/Remote Data and Authentication - Exercise/01.Messenger/app.js
+++ JS advanced/Remote Data and Authentication - Exercise/01.Messenger/app.js
@@ -1,5 +1,38 @@
 function attachEvents() {
-    console.log('TODO...');
+  const url = "http://localhost:3030/jsonstore/messenger";
+  let textArea = document.getElementById("messages");
+
+  let author = document.querySelector('input[name="author"]');
+  let content = document.querySelector('input[name="content"]');
+
+  document.getElementById("submit").addEventListener("click", send);
+  document.getElementById("refresh").addEventListener("click", refresh);
+
+  async function refresh() {
+    const res = await fetch(url);
+    const data = await res.json();
+    textArea.value = Object.values(data)
+      .map(({ author, content }) => `${author}: ${content}`)
+      .join("\n");
+  }
+  async function send() {
+    const newMsg = {
+      author: author.value,
+      content: content.value,
+    };
+    if (author.value != "" && content.value != "") {
+      fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(newMsg),
+      }).then(() => {
+        author.value = "";
+        content.value = "";
+      });
+    }
+  }
 }
 
 attachEvents();
