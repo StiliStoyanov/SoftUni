e4483465a90a572ab053ecb4771b88e6786e5e0e
Stiliyan Stoyanov <stilistoyanov1@gmail.com>
2022-07-25 16:36:06 +0300

    Added solution

---
+++ JS advanced/Client Side Rendering - Exercise/02.HTTP-Status-Cats/app.js
@@ -0,0 +1,33 @@
+import {html, render} from "../node_modules/lit-html/lit-html.js"
+import {cats} from "./catSeeder.js"
+
+let cardTemplate = (c) => html`
+<li>
+    <img src="./images/${c.imageLocation}.jpg" width="250" height="250">
+    <div class="info">
+         <button class="showBtn" @click=${onClick}>Show status code</button>
+         <div class="status" style="display: none" id="${c.id}">
+        <h4>Status Code: ${c.statusCode}</h4>
+        <p>${c.statusMessage}</p>
+    </div>
+</div>
+</li>
+`
+
+function onClick(e) {
+    let cat = e.target.parentNode
+    let result = cat.querySelector('.status').style.display
+    let showBtn = cat.querySelector('.showBtn')
+    if (result == 'block') {
+        cat.querySelector('.status').style.display = 'none'
+        showBtn.textContent = 'Show status code'
+    }
+    else{
+        cat.querySelector('.status').style.display = 'block'
+        showBtn.textContent = 'Hide status code'
+    }
+}
+
+let result = cats.map(cardTemplate)
+let main = document.getElementById('allCats')
+render(result, main)
