c69a8746df8d73aa781ddec47cc7ba49ced74e5c
Stiliyan Stoyanov <stilistoyanov1@gmail.com>
2022-08-04 17:50:59 +0300

    Final solution without Bonus

--- JS advanced/JS App Exam Prep/Meme_Lounge/src/app.js
+++ JS advanced/JS App Exam Prep/Meme_Lounge/src/app.js
@@ -1,22 +1,29 @@
+import { logout } from './api/users.js';
 import { page, render } from './lib.js';
 import { getUserData } from './util.js';
 import { catalogView } from './views/catalog.js';
+import { createView } from './views/create.js';
+import { detailsView } from './views/details.js';
+import { editView } from './views/edit.js';
 import { homeView } from './views/home.js';
 import { loginView } from './views/login.js';
+import { profileView } from './views/profile.js';
 import { registerView } from './views/register.js';
 
 const main = document.querySelector('main')
 
 page(decorateContext)
 
+document.getElementById('logoutBtn').addEventListener('click',onLogout)
+
 page('/', homeView)
 page('/memes', catalogView)
-page('/memes/:id', () => console.log('details'))
-page('/edit/:id', () => console.log('edit'))
+page('/memes/:id', detailsView)
+page('/edit/:id', editView)
 page('/login', loginView)
 page('/register', registerView)
-page('/create', () => console.log('create'))
-page('/profile', () => console.log('profile'))
+page('/create', createView)
+page('/profile', profileView)
 
 updateNav()
 page.start()
@@ -41,4 +48,10 @@ function updateNav() {
         document.querySelector('.user').style.display = 'none';
         document.querySelector('.guest').style.display = 'block';
     }
+}
+
+function onLogout() {
+    logout()
+    updateNav()
+    page.redirect('/')
 }
