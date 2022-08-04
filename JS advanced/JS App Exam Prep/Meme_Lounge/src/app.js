95dae96a3eb74490577ab120b407485b23c64a64
Stiliyan Stoyanov <stilistoyanov1@gmail.com>
2022-08-04 14:19:08 +0300

    partial solution

--- JS advanced/JS App Exam Prep/Meme_Lounge/src/app.js
+++ JS advanced/JS App Exam Prep/Meme_Lounge/src/app.js
@@ -1,4 +1,44 @@
-import * as api from './api/users.js'
+import { page, render } from './lib.js';
+import { getUserData } from './util.js';
+import { catalogView } from './views/catalog.js';
+import { homeView } from './views/home.js';
+import { loginView } from './views/login.js';
+import { registerView } from './views/register.js';
 
+const main = document.querySelector('main')
 
-window.api = api;
+page(decorateContext)
+
+page('/', homeView)
+page('/memes', catalogView)
+page('/memes/:id', () => console.log('details'))
+page('/edit/:id', () => console.log('edit'))
+page('/login', loginView)
+page('/register', registerView)
+page('/create', () => console.log('create'))
+page('/profile', () => console.log('profile'))
+
+updateNav()
+page.start()
+
+function decorateContext(ctx,next) {
+    ctx.render = renderMain
+    ctx.updateNav = updateNav
+    next()
+}
+
+function renderMain(templateResult) {
+    render(templateResult, main)
+}
+
+function updateNav() {
+    const userData = getUserData();
+    if (userData) {
+        document.querySelector('.user').style.display = 'block';
+        document.querySelector('.guest').style.display = 'none';
+        document.querySelector('.profile span').textContent = `Welcome, ${userData.email}`
+    } else {
+        document.querySelector('.user').style.display = 'none';
+        document.querySelector('.guest').style.display = 'block';
+    }
+}
